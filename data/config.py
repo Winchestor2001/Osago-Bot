9fe317481025b5773c6e5c7d000f3de8412edd59
Muzaffar <khodjayevmuzaffar@gmail.com>
2024-06-13 15:01:53 +0500

    d

--- data/config.py
+++ data/config.py
@@ -5,4 +5,10 @@ env = Env()
 env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
+DB_NAME = env.str("DB_NAME")
+DB_USER = env.str("DB_USER")
+DB_PASSWORD = env.str("DB_PASSWORD")
+DB_HOST = env.str("DB_HOST")
+DB_PORT = env.str("DB_PORT")
+
 # SUPPORT_ADMIN = env.str("SUPPORT_ADMIN")
---
+++ database/models.py
@@ -0,0 +1,40 @@
+from peewee import *
+from data.config import DB_NAME, DB_USER, DB_HOST, DB_PASSWORD, DB_PORT
+
+db = PostgresqlDatabase(DB_NAME, user=DB_USER, host=DB_HOST, password=DB_PASSWORD, port=DB_PORT)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Users(BaseModel):
+    user_id = BigIntegerField(primary_key=True, unique=True)
+    username = CharField(max_length=50, null=True)
+    full_name = CharField(max_length=155)
+    user_balance = FloatField(default=0)
+    from_link = CharField(max_length=50)
+    referals = IntegerField(default=0)
+
+    class Meta:
+        db_name = 'users'
+
+
+class Channels(BaseModel):
+    channel_id = BigIntegerField(primary_key=True)
+    channel_name = CharField(max_length=200, null=True)
+    channel_url = CharField(max_length=200, null=True)
+
+    class Meta:
+        db_name = 'channels'
+
+
+class Admins(BaseModel):
+    admin_id = BigIntegerField(primary_key=True, unique=True)
+
+    class Meta:
+        db_name = 'admins'
+
+
+models = [Users, Channels, Admins]
