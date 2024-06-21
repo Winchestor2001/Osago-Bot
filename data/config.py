0e84995a8c5657569d629aa4e857ecc687eec23b
sbexruz2001 <sbexruz2001@gmail.com>
2024-06-21 10:07:36 +0500

    add webhook

--- data/config.py
+++ data/config.py
@@ -6,6 +6,8 @@ env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
+WEBHOOK_URL = env.str("WEBHOOK_URL")
+WEBHOOK_PATH = env.str("WEBHOOK_PATH")
 DB_NAME = env.str("DB_NAME")
 DB_USER = env.str("DB_USER")
 DB_PASSWORD = env.str("DB_PASSWORD")
