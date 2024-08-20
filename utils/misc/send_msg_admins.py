d698a793ae4e9fec8e787e75f17e94537231857e
root <root@1079583-cp51749.tmweb.ru>
2024-08-20 12:29:47 +0300

    update

--- utils/misc/send_msg_admins.py
+++ utils/misc/send_msg_admins.py
@@ -8,4 +8,7 @@ async def send_to_admins(text):
     admins = [item['admin_id'] for item in admins if item.get("admin_id")]
     admins.extend(ADMINS)
     for i in admins:
-        await bot.send_message(chat_id=i, text=text)
+        try:
+            await bot.send_message(chat_id=i, text=text)
+        except:
+            continue
